1e4563e823a5c0752bc5dc2e7dc7a46e5fad09f4
Ngan Nguyen <ngannm19411c@st.uel.edu.vn>
2021-12-13 15:30:55 +0700

    import json

--- script/danhsach-sanpham.js
+++ script/danhsach-sanpham.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
 import { getDatabase, ref, onValue, child, query, orderByChild, equalTo} from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
-
+import data from '../data/firebase-config.json' assert { type: "json" };
 // let requestURL ="/data/firebase-config.json";
 // let request = new XMLHttpRequest();
 // request.open('GET', requestURL);
@@ -10,68 +10,10 @@ import { getDatabase, ref, onValue, child, query, orderByChild, equalTo} from "h
 // jQuery.getJSON("../data/firebase-config.json", function(data){
 
 // })
-
-fetch("../data/firebase-config.json")
-    .then(response => response.json())
-    .then(function(json){
-    const app = initializeApp(json)
-    const database = getDatabase(app);
-
-    //? bind document
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
-
-    $('#titleName').innerHTML = type;
-    $('#titleDescription').innerHTML = tiltleDes;
-    var myRef;
-    if (type === "Sản phẩm") {
-        var myRef = ref(database, 'NguyenLieu');
-    } else {
-        var myRef = query(ref(database, 'NguyenLieu') , orderByChild('productType'), equalTo(type));
-    }
- 
-    onValue(myRef, (snapshot) => {
-        
-        const productList =[]
-        let i=0;
-        
-        //láy dữ liệu về từ firebase
-        try {
-            snapshot.forEach(function(child){
-                productList.push(child.val())
-                i++;
-                if (i>7) throw 'break';  //forEach không hỗ trợ break nên dùng throw exception để break
-        })
-        }
-        catch{
-            //
-        }
-        
-        // đựa dữ liệu vào thẻ html
-        let htmls = productList.map(product => {
-            return `
-            <div class="col l-3 m-6 c-12 mg-t mg-r mg-l mg-b">
-                <a href="./san-pham.html?id=${product.productID}" class="product-container">
-                    <div class="product-img">
-                        <img src="${product.productImg}" alt="">
-                    </div>
-                    <div class="product-name">
-                        <p>${product.productName}</p>
-                    </div>
-                    <div class="product-price">
-                            <p>${product.productPrice}đ</p>
-                    </div>
-                </a>
-            </div>`
-        })
-        $('.product').innerHTML = htmls.join('');
-    });
-    })
+console.log(data)
 const params = new URLSearchParams(window.location.search)
 var type =Object.fromEntries(params.entries()).type
-console.log(type)
 var tiltleDes
-console.log(type)
 
 switch (type) {
     case "NguyenLieu" : {
@@ -95,7 +37,58 @@ switch (type) {
         break;
     }
 }
-console.log(type)
+const app = initializeApp(data)
+const database = getDatabase(app);
+
+//? bind document
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+$('#titleName').innerHTML = type;
+$('#titleDescription').innerHTML = tiltleDes;
+var myRef;
+if (type === "Sản phẩm") {
+    var myRef = ref(database, 'NguyenLieu');
+} else {
+    var myRef = query(ref(database, 'NguyenLieu') , orderByChild('productType'), equalTo(type));
+}
+
+onValue(myRef, (snapshot) => {
     
+    const productList =[]
+    let i=0;
+    
+    //láy dữ liệu về từ firebase
+    try {
+        snapshot.forEach(function(child){
+            productList.push(child.val())
+            i++;
+            if (i>7) throw 'break';  //forEach không hỗ trợ break nên dùng throw exception để break
+    })
+    }
+    catch{
+        //
+    }
+    
+    // đựa dữ liệu vào thẻ html
+    let htmls = productList.map(product => {
+        return `
+        <div class="col l-3 m-6 c-12 mg-t mg-r mg-l mg-b">
+            <a href="./san-pham.html?id=${product.productID}" class="product-container">
+                <div class="product-img">
+                    <img src="${product.productImg}" alt="">
+                </div>
+                <div class="product-name">
+                    <p>${product.productName}</p>
+                </div>
+                <div class="product-price">
+                        <p>${product.productPrice}đ</p>
+                </div>
+            </a>
+        </div>`
+    })
+    $('.product').innerHTML = htmls.join('');
+});
+
 // }
    
