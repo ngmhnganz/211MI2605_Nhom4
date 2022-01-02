7c790b586fad6f0a39c94690349a91ddbd18f667
Sunhuynh <xuanhk19411c@st.uel.edu.vn>
2022-01-02 16:35:06 +0700

    navi hoàn thiện 100%

--- script/header.js
+++ script/header.js
@@ -29,10 +29,7 @@
   htmlcssArrow.onclick = function() {
    navLinks.classList.toggle("show1");
   }
-  let moreArrow = document.querySelector(".more-arrow");
-  moreArrow.onclick = function() {
-   navLinks.classList.toggle("show2");
-  }
+
   let jsArrow = document.querySelector(".js-arrow");
   jsArrow.onclick = function() {
    navLinks.classList.toggle("show3");
