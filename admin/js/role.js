284e1fb942bd37216f710bdcde9eb3c6dc14708c
Anh Thi <thivna19411c@st.uel.edu.vn>
2022-01-07 02:17:12 +0700

    xu li 3/4 button

--- admin/js/role.js
+++ admin/js/role.js
@@ -1,26 +1,3 @@
-        // var parser, xmlDoc;
-        // var text="<nhanviens><nhanvien><MaNV> K19411629</MaNV><HoTen> Võ Nguyễn Anh Thi</HoTen> <TK> thivna19411c@uel.edu.vn</TK><ChucVu> Sinh Viên </ChucVu><Noilv> UEL </Noilv></nhanvien><nhanvien><MaNV> K19411632</MaNV><HoTen> Vũ Nguyễn Anh Thư</HoTen><TK>thuvna19411c@uel.edu.vn </TK><ChucVu> Sinh Viên </ChucVu><Noilv> UEL </Noilv></nhanvien><nhanvien><MaNV> K19411640</MaNV><HoTen> Huỳnh Kim Xuân </HoTen><TK> xuanhk19411c@uel.edu.vn</TK><ChucVu> Sinh Viên </ChucVu><Noilv> UEL </Noilv></nhanvien><nhanvien><MaNV> K19411615</MaNV><HoTen> Nguyễn Minh Ngân </HoTen><TK>ngannm19411c@uel.edu.vn </TK><ChucVu> Sinh Viên </ChucVu><Noilv> UEL </Noilv></nhanvien><nhanvien><MaNV> K19411626</MaNV><HoTen> Trần Thị Anh Phương </HoTen><TK>phuongtta19411c@uel.edu.vn</TK><ChucVu> Sinh Viên </ChucVu><Noilv> UEL </Noilv></nhanvien></nhanviens>";
-        // parser= new DOMParser();
-        // xmlDoc=parser.parseFromString(text,"text/xml");
-        // function show(){
-        // var x,i,table;
-        // table="<tr><th class='col c-1'> Mã NV </th><th class='col c-2'> Tên nhân viên </th><th class='col c-3' > Tài khoản </th><th class='col c-2'> Chức vụ </th><th class='col c-2'> Nơi làm việc </th><td class='col c-1'> <button class='btn' id='Change'> Sửa</button></td><td class='col c-1'> <button class='btn' id='Delete'> Xóa</button></td></tr>"
-        // x=xmlDoc.getElementsByTagName("nhanvien")
-        // for(i = 0; i < x.legth; i++){
-        //     table +="<tr><td class='col c-1' >"+
-        //                 x[i].getElementsByTagName("MaNV")[0].childNodes[0].nodeValue+
-        //                 "</td><td class='col c-2'>"+
-        //                 x[i].getElementsByTagName("HoTen")[0].childNodes[0].nodeValue+
-        //                 "</td><td class='col c-3'>"+
-        //                 x[i].getElementsByTagName("TK")[0].childNodes[0].nodeValue+
-        //                 "</td><td class='col c-2'>"+
-        //                 x[i].getElementsByTagName("ChucVu")[0].childNodes[0].nodeValue+
-        //                 "</td><td class='col c-2'>"+
-        //                 x[i].getElementsByTagName("Noilv")[0].childNodes[0].nodeValue+
-        //                 "</td></tr>"
-        // }
-        // document.getElementById("tblRole").innerHTML=table;
-        // }
         function show(){
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function(){
@@ -38,19 +15,24 @@
             var xmlDoc = xml.responseXML;
             var x = xmlDoc.getElementsByTagName("nhanvien");
             var nhanvien = "";
-            nhanvien="<tr><th class='col c-1'> Mã NV </th><th class='col c-2'> Tên nhân viên </th><th class='col c-3' > Tài khoản </th><th class='col c-2'> Chức vụ </th><th class='col c-2'> Nơi làm việc </th></tr>"
+            nhanvien="<tr><th class='col c-1'> Mã NV </th><th class='col c-2'> Tên nhân viên </th><th class='col c-3' > Tài khoản </th><th class='col c-2'> Chức vụ </th><th class='col c-2'> Nơi làm việc </th> <th></th><th></th> </tr>"
             for (i=0; i<x.length; i++){
-            nhanvien += "<tr><td class='col c-1' >"+
+            nhanvien += "<tr id='data'><td class='col c-1'onClick='red(id)' >"+
                             x[i].getElementsByTagName("MaNV")[0].childNodes[0].nodeValue+
-                            "</td><td class='col c-2'>"+
+                            "</td><td class='col c-2'style='text-align: left;'>"+
                             x[i].getElementsByTagName("HoTen")[0].childNodes[0].nodeValue+
-                            "</td><td class='col c-3'>"+
+                            "</td><td class='col c-3' style='text-align: left;'> "+
                             x[i].getElementsByTagName("TK")[0].childNodes[0].nodeValue+
                             "</td><td class='col c-2'>"+
                             x[i].getElementsByTagName("ChucVu")[0].childNodes[0].nodeValue+
                             "</td><td class='col c-2'>"+
                             x[i].getElementsByTagName("Noilv")[0].childNodes[0].nodeValue+
-                            "</td><td class='col c-1'> <button class='btn' id='Change'> Sửa</button></td><td class='col c-1'> <button class='btn' id='Delete'> Xóa</button></td></tr>"
+                            "</td><td class='col c-1'> <button class='btn Change'> Sửa</button></td><td class='col c-1'> <button class='btn Delete'> Xóa</button></td></tr>"
         }
         document.getElementById("tblRole").innerHTML = nhanvien;
     }
+    $(document).ready(function(){
+        $("#tblRole").on('click','.Delete',function(){
+            $(this).closest('tr').remove();
+          });
+    });
