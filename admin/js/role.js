c880a185b6fc9e69040bc4ec485d3dee8ec07102
Anh Thi <thivna19411c@st.uel.edu.vn>
2022-01-07 15:25:19 +0700

    done

--- admin/js/role.js
+++ admin/js/role.js
@@ -17,7 +17,7 @@
             var nhanvien = "";
             nhanvien="<tr><th class='col c-1'> Mã NV </th><th class='col c-2'> Tên nhân viên </th><th class='col c-3' > Tài khoản </th><th class='col c-2'> Chức vụ </th><th class='col c-2'> Nơi làm việc </th> <th></th><th></th> </tr>"
             for (i=0; i<x.length; i++){
-            nhanvien += "<tr id='data'><td class='col c-1'onClick='red(id)' >"+
+            nhanvien += "<tr id='data'><td class='col c-1'>"+
                             x[i].getElementsByTagName("MaNV")[0].childNodes[0].nodeValue+
                             "</td><td class='col c-2'style='text-align: left;'>"+
                             x[i].getElementsByTagName("HoTen")[0].childNodes[0].nodeValue+
@@ -27,12 +27,41 @@
                             x[i].getElementsByTagName("ChucVu")[0].childNodes[0].nodeValue+
                             "</td><td class='col c-2'>"+
                             x[i].getElementsByTagName("Noilv")[0].childNodes[0].nodeValue+
-                            "</td><td class='col c-1'> <button class='btn Change'> Sửa</button></td><td class='col c-1'> <button class='btn Delete'> Xóa</button></td></tr>"
+                            "</td><td class='col c-2'> <i class='fas fa-edit'></i><i class='fas fa-user-minus'></i></td></tr>"
         }
         document.getElementById("tblRole").innerHTML = nhanvien;
     }
     $(document).ready(function(){
-        $("#tblRole").on('click','.Delete',function(){
-            $(this).closest('tr').remove();
+        $("#tblRole").on('click','i.fas.fa-user-minus',function(){
+            $(this).closest('tr').remove().draw();
           });
+    $("#tblRole").on('mousedown.edit', "i.fas.fa-edit", function(e) {
+
+        $(this).removeClass().addClass("fas fa-save");
+        var $row = $(this).closest("tr").off("mousedown");
+        var $tds = $row.find("td").not(':first').not(':last');
+
+        $.each($tds, function(i, el) {
+          var txt = $(this).text();
+          $(this).html("").append("<input type='text' value=\""+txt+"\">");
+        });
+
+      });
+
+      $("#tblRole").on('mousedown', "input", function(e) {
+        e.stopPropagation();
+      });
+
+      $("#tblRole").on('mousedown.save', "i.fas.fa-save", function(e) {
+        
+        $(this).removeClass().addClass("fas fa-edit");
+        var $row = $(this).closest("tr");
+        var $tds = $row.find("td").not(':first').not(':last');
+        
+        $.each($tds, function(i, el) {
+          var txt = $(this).find("input").val()
+          $(this).html(txt);
+        });
+      });
     });
+    
